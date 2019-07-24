27bfb287b45470c0de8f81369ae17407073e7efe
Steve Mbouadeu <mbouadeus@gmail.com>
2019-07-24 16:33:42 -0700

    implement filters

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ import os
 import jinja2
 from google.appengine.api import urlfetch, users
 import json
+from py import apis, handlers
+
 
 jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
+
+def main_page_input(filters):
+    return { # retrieving all main page template parameters
+        'login_msg': get_login_msg(),
+        'login_url': get_login_url(),
+        'login_status': get_login_status(),
+        'restaurants': parse_restaurants(get_restaurants(get_coordinates(), filters), filters)
+}
+
+def get_coordinates():
+    api_key = 'AIzaSyBaL3Iw07VGFL5-PklkXrYas6lwi8NQQno'
+    url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + api_key
+
+    response = json.loads(urlfetch.fetch(url, method="POST").content)
+
+    return [response['location']['lat'], response['location']['lng']]
 
 def get_login_msg():
     if users.get_current_user():
@@ -27,36 +45,43 @@ def get_login_status():
     else:
         return 'login'
 
-def get_coordinates():
-    api_key = 'AIzaSyBaL3Iw07VGFL5-PklkXrYas6lwi8NQQno'
-    url = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + api_key
 
-    response = json.loads(urlfetch.fetch(url, method="POST").content)
+def get_restaurants(coordinates, filters):
+    if filters:
+        radius = str(int(filters['radius']) * 1610) # converting meters to miles
+    else:
+        radius = '8047' # default radius
 
-    return [response['location']['lat'], response['location']['lng']]
+    return json.loads(urlfetch.fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + str(coordinates[0]) + ',' + str(coordinates[1]) + '&radius=' + radius + '&type=cafe&key=AIzaSyBaL3Iw07VGFL5-PklkXrYas6lwi8NQQno').content)
 
-def get_restaurants(coordinates):
-    return json.loads(urlfetch.fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + str(coordinates[0]) + ',' + str(coordinates[1]) + '&radius=2000&type=restaurant&key=AIzaSyBaL3Iw07VGFL5-PklkXrYas6lwi8NQQno').content)
-
-def parse_restaurants(content):
+def parse_restaurants(content, filters):
     restaurants = []
 
-    for res in content['results']:
-        restaurants.append(res['name'])
+    if filters == None: # If no filters applied
+        for res in content['results']:
+            restaurants.append(res['name'])
+    else:
+        for res in content['results']:
+            if 'price_level' in res:
+                if res['price_level'] <= filters['price_level'] and res['opening_hours']['open_now']: #filter based on price levle and if currently open
+                    restaurants.append(res['name'])
 
     return restaurants
 
+import webapp2
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        content = get_restaurants(get_coordinates())
+        self.response.write(jinja_env.get_template('templates/main.html').render(main_page_input({
+            'price_level': 3,
+            'radius': '10',
+        })))
 
-        results_template = jinja_env.get_template('templates/main.html')
-        self.response.write(results_template.render({
-            'login_msg': get_login_msg(),
-            'login_url': get_login_url(),
-            'login_status': get_login_status(),
-            'restaurants': parse_restaurants(content)
-        }))
+    def post(self):
+        filters = {
+            'price_level': '2',
+            'radius': '100',
+        }
 
 class BreakfastHandler(webapp2.RequestHandler):
     def get(self):
@@ -72,26 +97,9 @@ class DinnerHandler(webapp2.RequestHandler):
 
 class SettingsHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Settings Page')
+        self.response.write(restaurants.is_here)
 
 
-# class LoginHandler(webapp2.RequestHandler):
-#     def get(self):
-#         if users.get_current_user():
-#             self.redirect('/')
-#             print('Already logged in')
-#         else:
-#             print('Logging in')
-#             login_url = users.create_login_url('/') #Get time and pick (breakfast, lunch, or dinner)
-#
-# class LogoutHandler(webapp2.RequestHandler):
-#     def get(self):
-#         if users.get_current_user():
-#             print('Logging out')
-#             logout_url = users.create_logout_url('/')
-#         else:
-#             print('Already logged out')
-#             self.redirect('/')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
